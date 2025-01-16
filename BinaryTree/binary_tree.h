81d8bfb0b212be3ef698abf10b6735cdcce095ca
Niko Peikrishvili <niko.peikrishvili@gmail.com>
2025-01-16 19:41:30 +0400

    feat: binary tree headers

---
+++ BinaryTree/binary_tree.h
@@ -0,0 +1,17 @@
+#pragma once
+
+typedef struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+} Node;
+
+
+Node *createNode(int data);
+
+void insertNode(Node **root, int data);
+Node* getDeepestRightMostNode(Node* root);
+void deleteDeepestRightMostNode(Node* root, Node* dNode);
+void delete(Node** root, int data);
+Node* search(Node** root, int data);
+void traversal(Node* root);
