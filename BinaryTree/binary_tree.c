c4b5f94958f114d142bc3255d1e179971b0cfc5c
Niko Peikrishvili <niko.peikrishvili@gmail.com>
2025-01-18 22:52:23 +0400

    feat: insert node method

--- BinaryTree/binary_tree.c
+++ BinaryTree/binary_tree.c
@@ -10,3 +10,35 @@ Node *createNode(const int data) {
     newNode->right = NULL;
     return newNode;
 }
+
+
+void insertNode(Node **root, const int data) {
+
+    Node *newNode = createNode(data);
+
+    if (NULL == *root) {
+        *root = newNode;
+        return;
+    }
+    int front = -1, rear = -1;
+    Node *queue[100];
+    queue[++rear] = *root;
+    while (front != rear) {
+        Node *temp = queue[++front];
+
+        if (NULL == temp->left) {
+            temp->left = newNode;
+            return;
+        }
+        else {
+            queue[++rear] = temp->left;
+        }
+        if (NULL == temp->right) {
+            temp->right = newNode;
+            return;
+        }
+        else {
+            queue[++rear] = temp->right;
+        }
+    }
+}
