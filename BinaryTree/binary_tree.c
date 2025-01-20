30d4885a1221e6082036a22aa7f83ec69db82822
Niko Peikrishvili <niko.peikrishvili@gmail.com>
2025-01-20 19:55:45 +0400

    feat: added deleteDeepestRightmostNode method

--- BinaryTree/binary_tree.c
+++ BinaryTree/binary_tree.c
@@ -4,16 +4,14 @@
 
 
 Node *createNode(const int data) {
-    Node *newNode = (Node*) malloc(sizeof(Node));
+    Node *newNode = (Node *) malloc(sizeof(Node));
     newNode->data = data;
     newNode->left = NULL;
     newNode->right = NULL;
     return newNode;
 }
 
-
 void insertNode(Node **root, const int data) {
-
     Node *newNode = createNode(data);
 
     if (NULL == *root) {
@@ -29,22 +27,21 @@ void insertNode(Node **root, const int data) {
         if (NULL == temp->left) {
             temp->left = newNode;
             return;
-        }
-        else {
+        } else {
             queue[++rear] = temp->left;
         }
         if (NULL == temp->right) {
             temp->right = newNode;
             return;
-        }
-        else {
+        } else {
             queue[++rear] = temp->right;
         }
     }
 }
-Node* getDeepestRightMostNode(Node* root) {
- Node *temp;
-    Node* queue[100];
+
+Node *getDeepestRightMostNode(Node *root) {
+    Node *temp;
+    Node *queue[100];
     int front = -1, rear = -1;
     queue[++rear] = root;
     while (front != rear) {
@@ -58,4 +55,40 @@ Node* getDeepestRightMostNode(Node* root) {
         }
     }
     return temp;
+}
+
+void deleteDeepestRightmostNode(Node *root, Node *dNode) {
+    Node *queue[100];
+    int front = -1, rear = -1;
+    queue[++rear] = root;
+
+    while (front != rear) {
+        Node *temp = queue[++front];
+
+        if (temp == dNode) {
+            temp = NULL;
+            free(dNode);
+            return;
+        }
+
+        if (temp->right != NULL) {
+            if (temp->right == dNode) {
+                temp->right = NULL;
+                free(dNode);
+                return;
+            } else {
+                queue[++rear] = temp->right;
+            }
+        }
+
+        if (temp->left != NULL) {
+            if (temp->left == dNode) {
+                temp->left = NULL;
+                free(dNode);
+                return;
+            } else {
+                queue[++rear] = temp->left;
+            }
+        }
+    }
 }
